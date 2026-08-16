5fa739cac48e3a7bf7bb14321166cb9acccbc410
Wip-Sama <47224042+Wip-Sama@users.noreply.github.com>
2026-08-17 01:56:48 +0200

    hopefully working release pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,11 @@ plugins {
 	kotlin("jvm") version "2.1.10" apply false
 }
 
+val projectVersion = providers.gradleProperty("version").orNull?.takeIf { it != "unspecified" } ?: "1.0.0-SNAPSHOT"
+
 allprojects {
 	group = "org.wip"
-	version = "1.0.0-SNAPSHOT"
+	version = projectVersion
 
 	repositories {
 		mavenCentral()
